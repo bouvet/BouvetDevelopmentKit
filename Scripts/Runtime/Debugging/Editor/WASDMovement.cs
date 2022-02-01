656735da43d69bacbaf48a834fb572a30bcab252
Ole Petter Larsen <ole.larsen@bouvet.no>
2022-02-01 14:02:53 +0100

    Removed support for old input system in WASD.

--- Scripts/Runtime/Debugging/Editor/WASDMovement.cs
+++ Scripts/Runtime/Debugging/Editor/WASDMovement.cs
@@ -1,16 +1,11 @@
 ﻿using UnityEngine;
-#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
-
-#endif
 
 /// <summary>
 /// Editor script for moving the Hololens/Camera in the Unity Editor play mode for debugging purposes.
 /// </summary>
 public class WASDMovement : MonoBehaviour
 {
-#pragma warning disable CS0414
-
     [TextArea(4,5)]
     public string controls = ("Right click+hold to look around. \nWASD to WASD-move, Q/E to down/up. \nShift to boost. Speed is meters/sec.");
 
@@ -22,8 +17,6 @@ public class WASDMovement : MonoBehaviour
 #if UNITY_EDITOR
     private void Update()
     {
-    #if ENABLE_INPUT_SYSTEM
-
         speedMulti = 1.0f;
         if (Keyboard.current.leftShiftKey.isPressed || Keyboard.current.rightShiftKey.isPressed) { speedMulti = shiftSpeedMultiply; }
         actualSpeed = (speed * speedMulti) * Time.deltaTime;
@@ -35,23 +28,6 @@ public class WASDMovement : MonoBehaviour
 
         if (Keyboard.current.eKey.isPressed)        { transform.Translate(0, actualSpeed, 0, Space.Self); }
         if (Keyboard.current.qKey.isPressed)        { transform.Translate(0, -actualSpeed, 0, Space.Self); }
-
-    #else
-
-        speedMulti = 1.0f;
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) { speedMulti = shiftSpeedMultiply; }
-        actualSpeed = (speed * speedMulti) * Time.deltaTime;
-
-        if      (Input.GetKey(KeyCode.W))   { transform.Translate(0, 0, actualSpeed, Space.Self); }
-        else if (Input.GetKey(KeyCode.S))   { transform.Translate(0, 0, -actualSpeed, Space.Self); }
-        if      (Input.GetKey(KeyCode.A))   { transform.Translate(-actualSpeed, 0, 0, Space.Self); }
-        else if (Input.GetKey(KeyCode.D))   { transform.Translate(actualSpeed, 0, 0, Space.Self); }
-
-        if      (Input.GetKey(KeyCode.E))   { transform.Translate(0, actualSpeed, 0, Space.Self); }
-        else if (Input.GetKey(KeyCode.Q))   { transform.Translate(0, -actualSpeed, 0, Space.Self); }
-
-    #endif
     }
 #endif
-#pragma warning restore CS0414
 }
